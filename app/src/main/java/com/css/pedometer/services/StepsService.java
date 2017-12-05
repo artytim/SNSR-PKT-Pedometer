a9aa1d26f1161c31b13a444a34a42bb5651752e3
artytim <arty.tim@gmail.com>
2017-12-05 12:29:16 +0800

    Fix some bugs in StepsDBHelper

---
+++ app/src/main/java/com/css/pedometer/services/StepsService.java
@@ -0,0 +1,59 @@
+package com.css.pedometer.services;
+
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+
+import com.css.pedometer.database.StepsDBHelper;
+
+public class StepsService extends Service implements SensorEventListener {
+
+    SensorManager mSensorManager;
+    Sensor mStepDetectorSensor;
+    StepsDBHelper mStepsDBHelper;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mSensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR) != null) {
+            mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            mSensorManager.registerListener(this, mStepDetectorSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+            mStepsDBHelper = new StepsDBHelper(getApplicationContext());
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        //Tim: https://stackoverflow.com/questions/9093271/start-sticky-and-start-not-sticky
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        mStepsDBHelper.createStepsEntry();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+}
